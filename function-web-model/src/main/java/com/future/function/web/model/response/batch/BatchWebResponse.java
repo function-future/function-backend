6707c26e513190ebc70d2f65afadb5483b60dfd6
Jonathan <jonathan16998@gmail.com>
2019-03-30 12:31:25 +0700

    Added response & response mapper for Batch

---
+++ function-web-model/src/main/java/com/future/function/web/model/response/batch/BatchWebResponse.java
@@ -0,0 +1,12 @@
+package com.future.function.web.model.response.batch;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class BatchWebResponse {
+
+    private long batchNumber;
+
+}
---
+++ function-web/src/main/java/com/future/function/web/mapper/response/BatchResponseMapper.java
@@ -0,0 +1,31 @@
+package com.future.function.web.mapper.response;
+
+import com.future.function.model.entity.feature.batch.Batch;
+import com.future.function.web.model.base.DataResponse;
+import com.future.function.web.model.base.PagingResponse;
+import com.future.function.web.model.response.batch.BatchWebResponse;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BatchResponseMapper {
+
+  public static DataResponse<BatchWebResponse> toBatchDataResponse(Batch batch) {
+
+    return DataResponse.<BatchWebResponse>builder()
+        .code(HttpStatus.CREATED.value())
+        .status(HttpStatus.CREATED.getReasonPhrase())
+        .data(new BatchWebResponse(batch.getNumber()))
+        .build();
+  }
+
+  public static PagingResponse<Long> toBatchesPagingResponse(List<Batch> batches) {
+
+    return PagingResponse.<Long>builder()
+        .code(HttpStatus.OK.value())
+        .status(HttpStatus.OK.getReasonPhrase())
+        .data(batches.stream().map(Batch::getNumber).collect(Collectors.toList()))
+        .build();
+  }
+}
