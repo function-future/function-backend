05f7bf9cc159a51b7e56930cdd4be34abe8701c9
Jonathan <jonathan16998@gmail.com>
2019-04-01 19:47:27 +0700

    Removed files with prefix Initial

--- function-service-impl/src/main/java/com/future/function/service/impl/helper/InitialServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.future.function.service.impl.helper;
-
-import com.future.function.service.api.helper.InitialService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class InitialServiceImpl implements InitialService {}
