88368ad4b0630b06aec5a8db9978a7ff0f96d5ba
Jonathan <jonathan16998@gmail.com>
2019-04-05 20:27:28 +0700

    Added @SuppressWarnings to overcome bug detection in Sonar. To be replaced after further discussion with mentor and teammates

--- function-common/src/main/java/com/future/function/common/exception/BadRequestException.java
+++ function-common/src/main/java/com/future/function/common/exception/BadRequestException.java
@@ -5,6 +5,7 @@ import javax.validation.ConstraintViolationException;
 import java.util.Collections;
 import java.util.Set;
 
+@SuppressWarnings("squid:MaximumInheritanceDepth")
 public class BadRequestException extends ConstraintViolationException {
   
   public BadRequestException(String message) {
