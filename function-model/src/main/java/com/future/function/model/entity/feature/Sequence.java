4af257e7f0c6bdbb5ee37a68c363daf45bff5cc5
Jonathan <jonathan16998@gmail.com>
2019-04-13 16:47:44 +0700

    Added `Sequence` class for auto incrementing sequence in objects

---
+++ function-model/src/main/java/com/future/function/model/entity/feature/Sequence.java
@@ -0,0 +1,29 @@
+package com.future.function.model.entity.feature;
+
+import com.future.function.model.entity.base.BaseEntity;
+import com.future.function.model.util.constant.DocumentName;
+import com.future.function.model.util.constant.FieldName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Document(collection = DocumentName.SEQUENCE)
+public class Sequence extends BaseEntity {
+  
+  @Id
+  private String id;
+  
+  @Field(value = FieldName.SEQUENCE_NUMBER)
+  private long sequenceNumber;
+  
+}
