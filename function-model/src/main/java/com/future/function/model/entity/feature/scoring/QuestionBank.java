4a6a4e1c80c7eb13e645e61c2ee45d7af95aed8c
oliverrsebastian <40243230+oliverrsebastian@users.noreply.github.com>
2019-05-09 23:43:22 +0700

    question and answers initialized

--- function-model/src/main/java/com/future/function/model/entity/feature/scoring/QuestionBank.java
+++ function-model/src/main/java/com/future/function/model/entity/feature/scoring/QuestionBank.java
@@ -2,15 +2,10 @@ package com.future.function.model.entity.feature.scoring;
 
 import com.future.function.model.entity.base.BaseEntity;
 import com.future.function.model.util.constant.DocumentName;
-import com.future.function.model.util.constant.FieldName;
-import java.util.UUID;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.UUID;
 
 @EqualsAndHashCode(callSuper = false)
 @Data
@@ -20,10 +15,8 @@ import org.springframework.data.mongodb.core.mapping.Field;
 @Document(collection = DocumentName.QUESTION_BANK)
 public class QuestionBank extends BaseEntity {
 
-  @Field(value = FieldName.QuestionBank.ID)
   @Builder.Default
   private String id = UUID.randomUUID().toString();
 
-  @Field(value = FieldName.QuestionBank.DESCRIPTION)
   private String description;
 }
