6627322a0a172d1021c53fa2941039ea4ad9338c
oliverrsebastian <40243230+oliverrsebastian@users.noreply.github.com>
2019-05-29 00:54:41 +0700

    fixed code and create more unit test
    fixed code and create more unit test

---
+++ function-web/src/test/java/com/future/function/web/mapper/response/scoring/StudentQuizDetailResponseMapperTest.java
@@ -0,0 +1,36 @@
+package com.future.function.web.mapper.response.scoring;
+
+import com.future.function.model.entity.feature.scoring.StudentQuestion;
+import com.future.function.model.entity.feature.scoring.StudentQuizDetail;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudentQuizDetailResponseMapperTest {
+
+    private static final String STUDENT_QUIZ_ID = "student-quiz-id";
+    private static final String STUDENT_QUIZ_DETAIL_ID = "student-quiz-detail-id";
+
+    private StudentQuizDetail studentQuizDetail;
+    private StudentQuestion studentQuestion;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void toStudentQuizDetailWebResponse() {
+    }
+
+    @Test
+    public void toStudentQuestionWebResponses() {
+    }
+
+    @Test
+    public void toStudentQuizDetailWebResponse1() {
+    }
+}
