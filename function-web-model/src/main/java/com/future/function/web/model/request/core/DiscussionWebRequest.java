67157ed762b183933a3444df69334370db6972b7
Jonathan <jonathan16998@gmail.com>
2019-05-28 07:39:06 +0700

    Added early model and web model for discussion feature

---
+++ function-web-model/src/main/java/com/future/function/web/model/request/core/DiscussionWebRequest.java
@@ -0,0 +1,21 @@
+package com.future.function.web.model.request.core;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.NotBlank;
+
+/**
+ * Model representation for discussion web request.
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DiscussionWebRequest {
+  
+  @NotBlank(message = "NotBlank")
+  private String comment;
+  
+}
