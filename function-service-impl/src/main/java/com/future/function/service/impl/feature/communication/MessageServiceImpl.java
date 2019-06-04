06286eca26f6abf1bec05a93506f1643e87635ee
Priagung Satyagama <priagungsatyagama@gmail.com>
2019-06-04 17:51:09 +0700

    finished message service impl

--- function-service-impl/src/main/java/com/future/function/service/impl/feature/communication/MessageServiceImpl.java
+++ function-service-impl/src/main/java/com/future/function/service/impl/feature/communication/MessageServiceImpl.java
@@ -1,10 +1,17 @@
 package com.future.function.service.impl.feature.communication;
 
+import com.future.function.common.exception.NotFoundException;
 import com.future.function.model.entity.feature.communication.Message;
 import com.future.function.repository.feature.communication.MessageRepository;
+import com.future.function.service.api.feature.communication.ChatroomService;
 import com.future.function.service.api.feature.communication.MessageService;
+import com.future.function.service.api.feature.core.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * Author: PriagungSatyagama
@@ -15,19 +22,49 @@ public class MessageServiceImpl implements MessageService {
 
   private MessageRepository messageRepository;
 
+  private ChatroomService chatroomService;
+
+  private UserService userService;
+
+  @Autowired
+  public MessageServiceImpl(MessageRepository messageRepository, ChatroomService chatroomService, UserService userService) {
+    this.messageRepository = messageRepository;
+    this.chatroomService = chatroomService;
+    this.userService = userService;
+  }
+
   @Override
-  public Page<Message> getMessages(String chatroomId) {
-    return null;
+  public Page<Message> getMessages(String chatroomId, Pageable pageable) {
+    return Optional.of(chatroomId)
+            .map(chatroomService::getChatroom)
+            .map(chatroom -> messageRepository.findAllByChatroomOrderByCreatedAtDesc(chatroom, pageable))
+            .get();
   }
 
   @Override
   public Message getLastMessage(String chatroomId) {
-    return null;
+    return Optional.of(chatroomId)
+            .map(chatroomService::getChatroom)
+            .map(chatroom -> messageRepository.findFirstByChatroomOrderByCreatedAtDesc(chatroom))
+            .orElseThrow(() -> new NotFoundException("No message was found"));
   }
 
   @Override
   public Message createMessage(Message message) {
-    return null;
+    return Optional.of(message)
+            .map(this::setSender)
+            .map(this::setChatroom)
+            .get();
+  }
+
+  private Message setSender(Message message) {
+    message.setSender(userService.getUser(message.getSender().getId()));
+    return message;
+  }
+
+  private Message setChatroom(Message message) {
+    message.setChatroom(chatroomService.getChatroom(message.getChatroom().getId()));
+    return message;
   }
 
 }
