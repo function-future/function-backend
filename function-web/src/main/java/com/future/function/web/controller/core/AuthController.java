a087ce5604ea576749f513a6c85a0af086a00b20
Jonathan <jonathan16998@gmail.com>
2019-06-08 05:52:11 +0700

    Added web response & mapper for authentication

--- function-web/src/main/java/com/future/function/web/controller/core/AuthController.java
+++ function-web/src/main/java/com/future/function/web/controller/core/AuthController.java
@@ -1,14 +1,15 @@
 package com.future.function.web.controller.core;
 
 import com.future.function.common.enumeration.core.Role;
-import com.future.function.model.entity.feature.core.User;
 import com.future.function.service.api.feature.core.AuthService;
 import com.future.function.session.annotation.WithAnyRole;
 import com.future.function.session.model.Session;
 import com.future.function.web.mapper.helper.ResponseHelper;
+import com.future.function.web.mapper.response.core.AuthResponseMapper;
 import com.future.function.web.model.request.core.AuthWebRequest;
 import com.future.function.web.model.response.base.BaseResponse;
 import com.future.function.web.model.response.base.DataResponse;
+import com.future.function.web.model.response.feature.core.AuthWebResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -35,30 +36,24 @@ public class AuthController {
   
   @ResponseStatus(HttpStatus.OK)
   @PostMapping
-  public DataResponse<User> login(
+  public DataResponse<AuthWebResponse> login(
     @RequestBody
       AuthWebRequest request, HttpServletResponse response
   ) {
     
-    return ResponseHelper.toDataResponse(HttpStatus.OK,
-                                         authService.login(request.getEmail(),
-                                                           request.getPassword(),
-                                                           response
-                                         )
-    );
+    return AuthResponseMapper.toAuthDataResponse(
+      authService.login(request.getEmail(), request.getPassword(), response));
   }
   
   @ResponseStatus(HttpStatus.OK)
   @GetMapping
-  public DataResponse<User> getLoginStatus(
+  public DataResponse<AuthWebResponse> getLoginStatus(
     @WithAnyRole(roles = { Role.STUDENT, Role.MENTOR, Role.ADMIN, Role.JUDGE })
       Session session
   ) {
     
-    return ResponseHelper.toDataResponse(HttpStatus.OK,
-                                         authService.getLoginStatus(
-                                           session.getId())
-    );
+    return AuthResponseMapper.toAuthDataResponse(
+      authService.getLoginStatus(session.getId()));
   }
   
   @ResponseStatus(HttpStatus.OK)
