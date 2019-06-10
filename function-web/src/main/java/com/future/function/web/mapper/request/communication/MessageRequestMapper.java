743d1fe5a77e73f63e0e542d0c45dab6614c28db
Priagung Satyagama <priagungsatyagama@gmail.com>
2019-06-10 15:36:35 +0700

    finished request mapper for chatting

---
+++ function-web/src/main/java/com/future/function/web/mapper/request/communication/MessageRequestMapper.java
@@ -0,0 +1,41 @@
+package com.future.function.web.mapper.request.communication;
+
+import com.future.function.model.entity.feature.communication.chatting.Chatroom;
+import com.future.function.model.entity.feature.communication.chatting.Message;
+import com.future.function.model.entity.feature.core.User;
+import com.future.function.validation.RequestValidator;
+import com.future.function.web.model.request.communication.MessageRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Author: priagung.satyagama
+ * Created At: 1:30 PM 6/10/2019
+ */
+@Slf4j
+@Component
+public class MessageRequestMapper {
+
+    private final RequestValidator validator;
+
+    @Autowired
+    public MessageRequestMapper(RequestValidator validator) {
+        this.validator = validator;
+    }
+
+    public Message toMessage(MessageRequest request, String userId, String chatroomId) {
+        return toValidatedMessage(request, userId, chatroomId);
+    }
+
+    private Message toValidatedMessage(MessageRequest request, String userId, String chatroomId) {
+        validator.validate(request);
+
+        return Message.builder()
+                .chatroom(Chatroom.builder().id(chatroomId).build())
+                .sender(User.builder().id(userId).build())
+                .text(request.getMessage())
+                .build();
+    }
+
+}
