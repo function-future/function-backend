9b2c34b42ea8a7e52359ac035ac20a6e6b0ce968
Priagung Satyagama <priagungsatyagama@gmail.com>
2019-06-27 17:50:24 +0700

    bug null when get chatrooms

--- function-web/src/main/java/com/future/function/web/mapper/response/communication/ChatroomResponseMapper.java
+++ function-web/src/main/java/com/future/function/web/mapper/response/communication/ChatroomResponseMapper.java
@@ -123,7 +123,20 @@ public class ChatroomResponseMapper {
                         messageService.getLastMessage(content.getId()),
                         messageStatusService.getSeenStatus(content.getId(), userId))
                 )
-                .sorted((o1, o2) -> o2.getLastMessage().getTime().compareTo(o1.getLastMessage().getTime()))
+                .sorted((o1, o2) -> {
+                    if (o2.getLastMessage() == null && o1.getLastMessage() == null) {
+                        return 0;
+                    }
+                    else if (o2.getLastMessage() == null) {
+                        return -1;
+                    }
+                    else if (o1.getLastMessage() == null) {
+                        return 1;
+                    }
+                    else {
+                        return o2.getLastMessage().getTime().compareTo(o1.getLastMessage().getTime());
+                    }
+                })
                 .collect(Collectors.toList());
     }
 
