daaabbe473ad77c3ce702af50ad778f74defe11c
Priagung Satyagama <priagungsatyagama@gmail.com>
2019-07-06 14:39:10 +0700

    finished reminder and notification model

---
+++ function-model/src/main/java/com/future/function/model/entity/feature/communication/reminder/Notification.java
@@ -0,0 +1,38 @@
+package com.future.function.model.entity.feature.communication.reminder;
+
+import com.future.function.model.entity.base.BaseEntity;
+import com.future.function.model.entity.feature.core.User;
+import com.future.function.model.util.constant.DocumentName;
+import com.future.function.model.util.constant.FieldName;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+/**
+ * Author: PriagungSatyagama
+ * Created At: 13:52 06/07/2019
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Document(collection = DocumentName.NOTIFICATION)
+public class Notification extends BaseEntity {
+
+  @Id
+  private String id;
+
+  @Field(FieldName.Notification.TITLE)
+  private String title;
+
+  @Field(FieldName.Notification.CONTENT)
+  private String content;
+
+  @Field(FieldName.Notification.MEMBER)
+  @DBRef(lazy = true)
+  private User member;
+
+}
