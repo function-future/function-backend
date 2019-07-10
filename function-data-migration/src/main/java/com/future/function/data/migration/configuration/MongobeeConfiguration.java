c32a9125dec9679d1dfd7de3ede0cb19366ab2bc
Jonathan <33094792+jonathan016@users.noreply.github.com>
2019-07-10 17:50:55 +0700

    Revert "Set migration to be run based on properties value (migration.run) (#163)" (#166)
    This reverts commit a99edc46e6ac2b02a104cebbb3d2ace0c89bf097.

--- function-data-migration/src/main/java/com/future/function/data/migration/configuration/MongobeeConfiguration.java
+++ function-data-migration/src/main/java/com/future/function/data/migration/configuration/MongobeeConfiguration.java
@@ -1,27 +1,20 @@
 package com.future.function.data.migration.configuration;
 
 import com.github.mongobee.Mongobee;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MongobeeConfiguration {
   
-  private final String mongoUri;
+  @Value("${spring.data.mongodb.uri}")
+  private String mongoUri;
   
-  private final String dbName;
-  
-  public MongobeeConfiguration(MongoProperties mongoProperties) {
-    
-    this.mongoUri = mongoProperties.getUri();
-    this.dbName = mongoProperties.getDatabase();
-  }
+  @Value("${spring.data.mongodb.database}")
+  private String dbName;
   
   @Bean
-  @ConditionalOnProperty(prefix = "migration",
-                         name = "run")
   public Mongobee mongobee() {
     
     Mongobee runner = new Mongobee(mongoUri);
