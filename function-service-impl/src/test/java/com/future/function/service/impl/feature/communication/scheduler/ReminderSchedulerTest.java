dc6b712ff1af9787a724ca550e5f59627dcda9ee
Priagung Satyagama <priagungsatyagama@gmail.com>
2019-07-11 11:39:25 +0700

    improve coverage reminder scheduler test

--- function-service-impl/src/test/java/com/future/function/service/impl/feature/communication/scheduler/ReminderSchedulerTest.java
+++ function-service-impl/src/test/java/com/future/function/service/impl/feature/communication/scheduler/ReminderSchedulerTest.java
@@ -12,8 +12,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Collections;
 
 import static org.mockito.Matchers.any;
@@ -32,7 +34,7 @@ public class ReminderSchedulerTest {
   private static final User USER_2 = User.builder().id(USER_ID_2).build();
   private static final String REMINDER_ID_1 = "reminderId1";
   private static final String REMINDER_ID_2 = "reminderId2";
-  private static final Reminder REMINDER_1 = Reminder.builder()
+  private Reminder REMINDER_1 = Reminder.builder()
           .id(REMINDER_ID_1)
           .isRepeatedMonthly(true)
           .hour(LocalDateTime.now().getHour())
@@ -40,9 +42,30 @@ public class ReminderSchedulerTest {
           .minute(LocalDateTime.now().getMinute())
           .members(Arrays.asList(USER_1, USER_2))
           .build();
-  private static final Reminder REMINDER_2 = Reminder.builder()
+
+  private Reminder REMINDER_2 = Reminder.builder()
           .id(REMINDER_ID_2)
           .isRepeatedMonthly(false)
+          .hour(LocalDateTime.now().getHour())
+          .minute(LocalDateTime.now().getMinute())
+          .days(Collections.singletonList(LocalDateTime.now().getDayOfWeek()))
+          .members(Arrays.asList(USER_1, USER_2))
+          .build();
+
+  private Reminder REMINDER_3 = Reminder.builder()
+          .id(REMINDER_ID_2)
+          .isRepeatedMonthly(true)
+          .lastReminderSent(getOneMonthBeforeNow())
+          .hour(LocalDateTime.now().getHour())
+          .minute(LocalDateTime.now().getMinute())
+          .monthlyDate(LocalDateTime.now().getDayOfMonth())
+          .members(Arrays.asList(USER_1, USER_2))
+          .build();
+
+  private Reminder REMINDER_4 = Reminder.builder()
+          .id(REMINDER_ID_2)
+          .isRepeatedMonthly(false)
+          .lastReminderSent(getOneMonthBeforeNow())
           .hour(LocalDateTime.now().getHour())
           .minute(LocalDateTime.now().getMinute())
           .days(Collections.singletonList(LocalDateTime.now().getDayOfWeek()))
@@ -84,6 +107,36 @@ public class ReminderSchedulerTest {
     verify(reminderService).getAllReminder();
     verify(notificationService, times(2)).createNotification(any(Notification.class));
     verify(reminderService).updateReminder(REMINDER_2);
+  }
+
+  @Test
+  public void testGivenMethodCallByCallingCreateNotificationBasedOnReminderMonthlyLastSentNotNull() {
+    when(notificationService.createNotification(any(Notification.class))).thenReturn(Notification.builder().build());
+    when(reminderService.getAllReminder()).thenReturn(Collections.singletonList(REMINDER_3));
+
+    reminderScheduler.createNotificationBasedOnReminder();
+
+    verify(reminderService).getAllReminder();
+    verify(notificationService, times(2)).createNotification(any(Notification.class));
+    verify(reminderService).updateReminder(REMINDER_3);
+  }
+
+  @Test
+  public void testGivenMethodCallByCallingCreateNotificationBasedOnReminderWeeklyLastSeenNotNull() {
+    when(notificationService.createNotification(any(Notification.class))).thenReturn(Notification.builder().build());
+    when(reminderService.getAllReminder()).thenReturn(Collections.singletonList(REMINDER_4));
+
+    reminderScheduler.createNotificationBasedOnReminder();
+
+    verify(reminderService).getAllReminder();
+    verify(notificationService, times(2)).createNotification(any(Notification.class));
+    verify(reminderService).updateReminder(REMINDER_4);
+  }
+
+  private Long getOneMonthBeforeNow() {
+    Calendar calendar = Calendar.getInstance();
+    calendar.add(Calendar.MONTH, -1);
+    return calendar.getTimeInMillis();
   }
 
 }
