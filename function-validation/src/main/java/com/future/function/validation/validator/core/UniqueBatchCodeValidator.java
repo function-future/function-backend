dcb2c8d317d66b0ac16f5d28e708aa03266c15c1
Jonathan <jonathan16998@gmail.com>
2019-07-13 07:44:18 +0700

    Fixed batch code editing process

--- function-validation/src/main/java/com/future/function/validation/validator/core/UniqueBatchCodeValidator.java
+++ function-validation/src/main/java/com/future/function/validation/validator/core/UniqueBatchCodeValidator.java
@@ -1,15 +1,16 @@
 package com.future.function.validation.validator.core;
 
+import com.future.function.common.data.core.BatchData;
+import com.future.function.model.entity.feature.core.Batch;
 import com.future.function.repository.feature.core.BatchRepository;
 import com.future.function.validation.annotation.core.UniqueBatchCode;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-import java.util.Optional;
 
 public class UniqueBatchCodeValidator
-  implements ConstraintValidator<UniqueBatchCode, String> {
+  implements ConstraintValidator<UniqueBatchCode, BatchData> {
   
   @Autowired
   private BatchRepository batchRepository;
@@ -20,12 +21,12 @@ public class UniqueBatchCodeValidator
   }
   
   @Override
-  public boolean isValid(String code, ConstraintValidatorContext context) {
+  public boolean isValid(BatchData data, ConstraintValidatorContext context) {
     
-    return Optional.ofNullable(code)
-      .filter(s -> !batchRepository.findByCode(s)
-        .isPresent())
-      .isPresent();
+    return batchRepository.findByCode(data.getCode())
+      .map(Batch::getId)
+      .map(id -> id.equals(data.getId()))
+      .orElse(true);
   }
   
 }
