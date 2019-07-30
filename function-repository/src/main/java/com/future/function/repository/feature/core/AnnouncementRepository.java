ad8d4935a11e76ed53c9e8b37885c9e86d172b3d
Jonathan <jonathan16998@gmail.com>
2019-07-30 13:45:25 +0700

    Fixed unsorted announcement

--- function-repository/src/main/java/com/future/function/repository/feature/core/AnnouncementRepository.java
+++ function-repository/src/main/java/com/future/function/repository/feature/core/AnnouncementRepository.java
@@ -1,6 +1,8 @@
 package com.future.function.repository.feature.core;
 
 import com.future.function.model.entity.feature.core.Announcement;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,8 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface AnnouncementRepository
-  extends MongoRepository<Announcement, String> {}
+  extends MongoRepository<Announcement, String> {
+  
+  Page<Announcement> findAllByOrderByUpdatedAtDesc(Pageable pageable);
+  
+}
