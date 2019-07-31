b2e89ec143e290cfb9307cba39ed093be10fae42
Jonathan <33094792+jonathan016@users.noreply.github.com>
2019-07-31 08:03:20 +0700

    Bugfix/add paths in root folder migration (#210)
    * Add paths in root folder migration
    
    * Changed use of BasicDbObject to using User class for admin migration

--- function-model/src/main/java/com/future/function/model/util/constant/FieldName.java
+++ function-model/src/main/java/com/future/function/model/util/constant/FieldName.java
@@ -82,6 +82,8 @@ public interface FieldName {
     public static final String NAME = "name";
 
     public static final String PARENT_ID = "parentId";
+    
+    public static final String PATHS = "paths";
 
     public static final String USED = "used";
 
