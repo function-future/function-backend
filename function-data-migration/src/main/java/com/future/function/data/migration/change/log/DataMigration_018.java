59a1858874b1c7e28a507637d46d7f16c237b3ac
Jonathan <33094792+jonathan016@users.noreply.github.com>
2019-10-21 07:22:18 -0700

    [bugfix/CORE-81] Change login modal urlRegex (#290)

---
+++ function-data-migration/src/main/java/com/future/function/data/migration/change/log/DataMigration_018.java
@@ -0,0 +1,33 @@
+package com.future.function.data.migration.change.log;
+
+import com.future.function.model.entity.feature.core.Access;
+import com.future.function.model.util.constant.FieldName;
+import com.github.mongobee.changeset.ChangeLog;
+import com.github.mongobee.changeset.ChangeSet;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
+
+@ChangeLog(order = "018")
+public class DataMigration_018 {
+
+
+  @ChangeSet(author = "jonathan",
+             id = "changeUrlRegexForLoginModal",
+             order = "0001")
+  public void changeUrlRegexForLoginModal(MongoTemplate mongoTemplate) {
+
+    Criteria criteria = Criteria.where(FieldName.Access.URL_REGEX)
+      .is("^\\/login(\\/|\\?.*)?$");
+    List<Access> accesses = mongoTemplate.find(
+      Query.query(criteria), Access.class);
+
+    accesses.forEach(access -> {
+      access.setUrlRegex("^(\\/|)\\?auth=login$");
+      mongoTemplate.save(access);
+    });
+  }
+
+}
