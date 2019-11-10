998d5ee2b1bd3fbfb3a8fbf62e9b15be8b43138c
priagungs <priagungsatyagama@gmail.com>
2019-11-10 18:30:37 +0700

    add websocket config

---
+++ function-web/src/main/java/com/future/function/web/configuration/WebsocketConfiguration.java
@@ -0,0 +1,17 @@
+package com.future.function.web.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebsocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer {
+
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
+    stompEndpointRegistry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+  }
+}
