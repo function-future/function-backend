6c5389588ea0ac0388025b293aeb69a35cf500f5
priagungs <priagungsatyagama@gmail.com>
2019-11-10 18:33:35 +0700

    moved stomp endpoint to properties

--- function-web/src/main/java/com/future/function/web/configuration/WebsocketConfiguration.java
+++ function-web/src/main/java/com/future/function/web/configuration/WebsocketConfiguration.java
@@ -1,5 +1,6 @@
 package com.future.function.web.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
@@ -10,8 +11,11 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @EnableWebSocketMessageBroker
 public class WebsocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer {
 
+  @Value("${function.stomp-endpoint}")
+  private String endpoint;
+
   @Override
   public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
-    stompEndpointRegistry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+    stompEndpointRegistry.addEndpoint(endpoint).setAllowedOrigins("*").withSockJS();
   }
 }
