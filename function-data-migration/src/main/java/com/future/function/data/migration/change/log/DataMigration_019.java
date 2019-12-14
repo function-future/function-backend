14a33c8bc074023163b8755aecbff82fa19c126e
David William <david_wk98@yahoo.com>
2019-12-14 19:38:08 +0700

    Updated menuList for scoring-related pages
    Fix bug on migration

--- function-data-migration/src/main/java/com/future/function/data/migration/change/log/DataMigration_019.java
+++ function-data-migration/src/main/java/com/future/function/data/migration/change/log/DataMigration_019.java
@@ -2,86 +2,76 @@ package com.future.function.data.migration.change.log;
 
 import com.future.function.common.enumeration.core.Role;
 import com.future.function.model.entity.feature.core.Menu;
-import com.future.function.model.util.constant.DocumentName;
 import com.future.function.model.util.constant.FieldName;
 import com.github.mongobee.changeset.ChangeLog;
 import com.github.mongobee.changeset.ChangeSet;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Filters;
-import org.bson.Document;
-import org.bson.conversions.Bson;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Consumer;
 
-@SuppressWarnings(value = { "squid:S00101", "squid:S1192" })
 @ChangeLog(order = "019")
 public class DataMigration_019 {
 
     // Update menu list for scoring.
 
-    private Document removeDeprecatedMenuList (Document document) {
-        document.remove("questionBanks");
-        document.remove("quizzes");
-        document.remove("assignments");
-        document.remove("comparisons");
-        return document;
+    private Menu removeDeprecatedMenuList (Menu menu) {
+        menu.getSections().remove("questionBanks");
+        menu.getSections().remove("assignments");
+        menu.getSections().remove("quizzes");
+        return menu;
     }
 
     @ChangeSet(author = "david",
             id = "scoringUpdateStudentMenuList",
             order = "0001")
-    public void changeStudentMenuList(MongoDatabase mongoDatabase) {
-        MongoCollection<Document> menuCollection = mongoDatabase.getCollection(DocumentName.MENU);
-        Bson filter = Filters.eq(FieldName.Menu.ROLE, Role.STUDENT);
-        menuCollection
-                .find(Filters.eq(FieldName.Menu.ROLE, Role.STUDENT))
-                .forEach((Consumer<? super Document>) document -> {
-                    document = removeDeprecatedMenuList(document);
-                    document.put("grades", true);
-                    document.put("points", true);
-                    document.put("comparisons", false);
-                    menuCollection.findOneAndReplace(filter, document);
-                });
+    public void changeStudentMenuList(MongoTemplate mongoTemplate) {
+        Criteria criteria = Criteria.where(FieldName.Menu.ROLE)
+                .is(Role.STUDENT);
+
+        List<Menu> menus = mongoTemplate.find(Query.query(criteria), Menu.class);
+        menus.forEach(menu -> {
+            removeDeprecatedMenuList(menu);
+            menu.getSections().put("grades", true);
+            menu.getSections().put("points", true);
+            menu.getSections().put("comparisons", false);
+            mongoTemplate.save(menu);
+        });
     }
 
     @ChangeSet(author = "david",
             id = "scoringUpdateGuestMenuList",
             order = "0002")
-    public void changeGuestMenuList(MongoDatabase mongoDatabase) {
-        MongoCollection<Document> menuCollection = mongoDatabase.getCollection(DocumentName.MENU);
-        Bson filter = Filters.eq(FieldName.Menu.ROLE, Role.UNKNOWN);
-        menuCollection
-                .find(Filters.eq(FieldName.Menu.ROLE, Role.UNKNOWN))
-                .forEach((Consumer<? super Document>) document -> {
-                    document = removeDeprecatedMenuList(document);
-                    document.put("grades", false);
-                    document.put("points", false);
-                    document.put("comparisons", false);
-                    menuCollection.findOneAndReplace(filter, document);
-                });
+    public void changeGuestMenuList(MongoTemplate mongoTemplate) {
+        Criteria criteria = Criteria.where(FieldName.Menu.ROLE)
+                .is(Role.UNKNOWN);
+
+        List<Menu> menus = mongoTemplate.find(Query.query(criteria), Menu.class);
+        menus.forEach(menu -> {
+            removeDeprecatedMenuList(menu);
+            menu.getSections().put("grades", false);
+            menu.getSections().put("points", false);
+            menu.getSections().put("comparisons", false);
+            mongoTemplate.save(menu);
+        });
     }
 
     @ChangeSet(author = "david",
             id = "scoringUpdateOthersMenuList",
             order = "0003")
-    public void changeOthersMenuList(MongoDatabase mongoDatabase) {
-        MongoCollection<Document> menuCollection = mongoDatabase.getCollection(DocumentName.MENU);
-        Bson filter = Filters.regex(FieldName.Menu.ROLE, ".*(ADMIN|JUDGE|MENTOR).*");
-        menuCollection
-                .find(Filters.eq(FieldName.Menu.ROLE, Role.ADMIN))
-                .forEach((Consumer<? super Document>) document -> {
-                    document = removeDeprecatedMenuList(document);
-                    document.put("grades", true);
-                    document.put("points", false);
-                    document.put("comparisons", true);
-                    menuCollection.findOneAndReplace(filter, document);
-                });
+    public void changeOthersMenuList(MongoTemplate mongoTemplate) {
+        Criteria criteria = Criteria.where(FieldName.Menu.ROLE)
+                .regex(".*(ADMIN|JUDGE|MENTOR).*");
+
+        List<Menu> menus = mongoTemplate.find(Query.query(criteria), Menu.class);
+        menus.forEach(menu -> {
+            removeDeprecatedMenuList(menu);
+            menu.getSections().put("grades", true);
+            menu.getSections().put("points", false);
+            menu.getSections().put("comparisons", true);
+            mongoTemplate.save(menu);
+        });
     }
+
 }
