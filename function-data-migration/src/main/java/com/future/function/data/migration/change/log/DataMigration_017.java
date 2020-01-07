8dff3f0d6a73dbc50dd956e23d5c43d697c76c1c
oliverrsebastian <oliverrsebastian@gmail.com>
2020-01-07 17:52:20 +0700

    remove unused imports, remove unused version

--- function-data-migration/src/main/java/com/future/function/data/migration/change/log/DataMigration_017.java
+++ function-data-migration/src/main/java/com/future/function/data/migration/change/log/DataMigration_017.java
@@ -1,18 +1,11 @@
 package com.future.function.data.migration.change.log;
 
-import com.future.function.common.enumeration.core.Role;
 import com.future.function.model.util.constant.DocumentName;
-import com.future.function.model.util.constant.FieldName;
 import com.github.cloudyrock.mongock.ChangeLog;
 import com.github.cloudyrock.mongock.ChangeSet;
-import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.IndexOptions;
 import com.mongodb.client.model.Indexes;
-import java.util.function.Consumer;
-import org.bson.Document;
-import org.bson.conversions.Bson;
 
 import static com.future.function.data.migration.constant.IndexName.USER_ROLE_DELETED;
 import static com.future.function.data.migration.constant.IndexName.USER_ROLE_NAME_DELETED;
