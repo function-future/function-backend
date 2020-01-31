3f2809c9894549bf873e1e47b43c2fd49cf33a25
Jonathan <33094792+jonathan016@users.noreply.github.com>
2020-01-31 12:34:53 +0700

    [bugfix/CORE-95] FileDeleteScheduler not deleting file versions (#358)

--- function-service-impl/src/main/java/com/future/function/service/impl/feature/core/scheduler/FileDeleteScheduler.java
+++ function-service-impl/src/main/java/com/future/function/service/impl/feature/core/scheduler/FileDeleteScheduler.java
@@ -5,6 +5,7 @@ import com.future.function.model.entity.feature.core.FileV2;
 import com.future.function.repository.feature.core.FileRepositoryV2;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.FileSystemUtils;
 
 import java.io.File;
@@ -46,13 +47,33 @@ public class FileDeleteScheduler {
 
     Optional.of(fileV2)
       .filter(file -> !file.isMarkFolder())
-      .ifPresent(file -> FileSystemUtils.deleteRecursively(
-        new File(this.getContainingFolderPath(file))));
+      .ifPresent(this::deleteFileFromSystem);
   }
 
-  private String getContainingFolderPath(FileV2 fileV2) {
+  private void deleteFileFromSystem(FileV2 file) {
 
-    String filePath = fileV2.getFilePath();
+    if (CollectionUtils.isEmpty(file.getVersions())) {
+      this.deleteFileContainerFolder(
+        this.getContainingFolderPath(file.getFilePath()));
+    } else {
+      this.deleteFileVersions(file);
+    }
+  }
+
+  private void deleteFileVersions(FileV2 file) {
+
+    file.getVersions()
+      .values()
+      .forEach(version -> this.deleteFileContainerFolder(
+        this.getContainingFolderPath(version.getPath())));
+  }
+
+  private void deleteFileContainerFolder(String filePath) {
+
+    FileSystemUtils.deleteRecursively(new File(filePath));
+  }
+
+  private String getContainingFolderPath(String filePath) {
 
     return filePath.substring(0, filePath.lastIndexOf(File.separator));
   }
